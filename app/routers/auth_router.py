80eb5ee254d0a42aecce9969e69918d4c905e55c
mohits-git <mohitkhatri2805@gmail.com>
2026-01-06 06:13:06 +0000

    refac: use async-await in routers

--- app/routers/auth_router.py
+++ app/routers/auth_router.py
@@ -1,13 +1,24 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+from fastapi.responses import JSONResponse
 
-from app.dependencies import AuthServiceInstance
+from app.dependencies import AuthServiceInstance, AuthTokenHeader
 from app.dtos.auth import LoginRequest, LoginResponse
+from app.models.user import User
 
 
-auth_router = APIRouter(prefix="/api/auth")
+auth_router = APIRouter(prefix="/auth")
 
 
 @auth_router.post('/login', response_model=LoginResponse)
-def handle_login(login_request: LoginRequest, auth_service: AuthServiceInstance):
-    token = auth_service.login(**login_request.model_dump())
+async def handle_login(login_request: LoginRequest, auth_service: AuthServiceInstance):
+    token = await auth_service.login(**login_request.model_dump())
     return LoginResponse(token=token)
+
+
+@auth_router.post('/me', response_model=User)
+async def handle_auth_me(token: AuthTokenHeader, auth_service: AuthServiceInstance):
+    user = await auth_service.get_current_user(token)
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content=user.model_dump(mode='json', exclude_none=True)
+    )
