50bfab3adf8fdc3ca63f8bb985a3487a1ff12dce
mohits-git <mohitkhatri2805@gmail.com>
2026-01-06 10:34:23 +0000

    feat: add user service and update auth service for error handling

--- app/services/auth.py
+++ app/services/auth.py
@@ -1,12 +1,16 @@
+from app.errors.app_exception import AppException
+from app.errors.codes import AppErr
 from app.interfaces import PasswordHasher, TokenProvider, UserRepository
 from app.models.user import User, UserClaims
 
 
 class AuthService:
-    def __init__(self,
-                 user_repo: UserRepository,
-                 token_provider: TokenProvider,
-                 password_hasher: PasswordHasher):
+    def __init__(
+        self,
+        user_repo: UserRepository,
+        token_provider: TokenProvider,
+        password_hasher: PasswordHasher,
+    ):
         self._user_repo = user_repo
         self._token_provider = token_provider
         self._password_hasher = password_hasher
@@ -17,15 +21,16 @@ class AuthService:
         :returns token_string
         """
         user = await self._user_repo.get_by_email(email)
-        if user is None:
-            raise Exception("Not Found")
-
-        if not user.password or not self._password_hasher.verify_password(
-                password, user.password):
-            raise Exception("Forbidden")
+        if (
+            user is None
+            or not user.password
+            or not self._password_hasher.verify_password(password, user.password)
+        ):
+            raise AppException(AppErr.INVALID_USER_CREDENTIALS)
 
         token = self._token_provider.generate_token(
-            UserClaims.model_validate(**user.model_dump(), extra="ignore"))
+            UserClaims.model_validate(**user.model_dump(), extra="ignore")
+        )
         return token
 
     async def get_current_user(self, token: str) -> User:
@@ -33,7 +38,7 @@ class AuthService:
         user = await self._user_repo.get(user_claims.user_id)
         if user is None:
             raise Exception("Invalid user token")
-        user.password = None
+        user.password = ''
         return user
 
     def logout(self, token: str) -> None:
---
+++ app/services/user.py
@@ -0,0 +1,70 @@
+import uuid
+from app.errors.app_exception import AppException
+from app.errors.codes import AppErr
+from app.interfaces import PasswordHasher, UserRepository, ProjectRepository
+from app.models.user import User, UserClaims, UserRole
+
+
+class UserService:
+    def __init__(
+        self,
+        password_hasher: PasswordHasher,
+        user_repo: UserRepository,
+        project_repo: ProjectRepository,
+    ):
+        self.password_hasher = password_hasher
+        self.user_repo = user_repo
+        self.project_repo = project_repo
+
+    async def create_user(self, curr_user: UserClaims, user: User) -> str:
+        if curr_user.role != UserRole.Admin:
+            raise AppException(AppErr.ADMIN_ONLY)
+        if not user.password:
+            raise AppException(AppErr.CREATE_USER_PASSWORD_REQUIRED)
+        hashed_password = self.password_hasher.hash_password(user.password)
+        user.password = hashed_password
+        user.id = uuid.uuid4().hex
+        await self.user_repo.save(user)
+        return user.id
+
+    async def update_user(self, curr_user: UserClaims, user: User) -> None:
+        if curr_user.role != UserRole.Admin:
+            raise AppException(AppErr.ADMIN_ONLY)
+        if user.password != "":
+            hashed_password = self.password_hasher.hash_password(user.password)
+            user.password = hashed_password
+        await self.user_repo.update(user)
+
+    async def get_user_by_id(self, curr_user: UserClaims, user_id: str) -> User:
+        if curr_user.role != UserRole.Admin and curr_user.user_id != user_id:
+            raise AppException(AppErr.ADMIN_ONLY)
+        user = await self.user_repo.get(user_id)
+        if not user:
+            raise AppException(AppErr.USER_NOT_FOUND)
+        return user
+
+    async def get_all_users(self, curr_user: UserClaims) -> list[User]:
+        if curr_user.role != UserRole.Admin:
+            raise AppException(AppErr.ADMIN_ONLY)
+        return await self.user_repo.get_all()
+
+    async def delete_user(self, curr_user: UserClaims, user_id: str):
+        if curr_user.role != UserRole.Admin:
+            raise AppException(AppErr.ADMIN_ONLY)
+        if curr_user.user_id == user_id:
+            raise AppException(AppErr.CANNOT_DELETE_SELF)
+        await self.user_repo.delete(user_id)
+
+    async def get_user_budget(self, curr_user: UserClaims) -> float:
+        user = await self.user_repo.get(curr_user.user_id)
+        if user is None:
+            raise AppException(AppErr.USER_NOT_FOUND)
+
+        if user.project_id == "":
+            return 0.0
+
+        project = await self.project_repo.get(user.project_id)
+        if project is None:
+            return 0.0
+
+        return float(project.budget)
