636006d10f81aed67a1df2f4f55deb69f88221b9
mohits-git <mohitkhatri2805@gmail.com>
2026-01-08 02:24:24 +0530

    feat: add cors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
+import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from uvicorn import run
+from fastapi.middleware.cors import CORSMiddleware
+from app.dtos.response import ErrorResponse
+from app.errors.app_exception import AppException
+from app.errors.mapping import ERROR_MAP
 from app.lifespan import lifespan
 from app.routers.auth import auth_router
 from app.routers.user import user_router
@@ -8,9 +12,6 @@ from app.routers.project import project_router
 from app.routers.department import department_router
 from app.routers.expense import expense_router
 from app.routers.advance import advance_router
-from app.errors.app_exception import AppException
-from app.errors.mapping import ERROR_MAP
-from app.dtos.response import ErrorResponse
 
 
 app = FastAPI(lifespan=lifespan)
@@ -21,9 +22,24 @@ app.include_router(department_router, prefix="/api")
 app.include_router(expense_router, prefix="/api")
 app.include_router(advance_router, prefix="/api")
 
+origins = [
+    "https://watchexpense.mohits.me",
+    "http://localhost:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.exception_handler(AppException)
 async def app_exception_handler(request, exc: AppException):
+    # TODO: remove after dev
+    print(exc.err_code, exc.message, exc.cause)
     http_status, default_msg = ERROR_MAP.get(
         exc.err_code, (500, "Unknown error"))
     return JSONResponse(
@@ -31,10 +47,9 @@ async def app_exception_handler(request, exc: AppException):
         content=ErrorResponse(
             status=exc.err_code.value,
             message=exc.message or default_msg
-        )
+        ).model_dump()
     )
 
 
 if __name__ == "__main__":
-    print("Running server with Uvicorn")
-    run(app)
+    uvicorn.run(app)
