312620bd7399d44c95d57c40a6dd0d92c4922154
mohits-git <mohitkhatri2805@gmail.com>
2026-01-08 01:10:23 +0530

    feat: add AppException handler

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from uvicorn import run
 from app.lifespan import lifespan
 from app.routers.auth import auth_router
@@ -7,6 +8,9 @@ from app.routers.project import project_router
 from app.routers.department import department_router
 from app.routers.expense import expense_router
 from app.routers.advance import advance_router
+from app.errors.app_exception import AppException
+from app.errors.mapping import ERROR_MAP
+from app.dtos.response import ErrorResponse
 
 
 app = FastAPI(lifespan=lifespan)
@@ -16,6 +20,19 @@ app.include_router(project_router, prefix="/api")
 app.include_router(department_router, prefix="/api")
 app.include_router(expense_router, prefix="/api")
 app.include_router(advance_router, prefix="/api")
+
+
+@app.exception_handler(AppException)
+async def app_exception_handler(request, exc: AppException):
+    http_status, default_msg = ERROR_MAP.get(
+        exc.err_code, (500, "Unknown error"))
+    return JSONResponse(
+        status_code=http_status,
+        content=ErrorResponse(
+            status=exc.err_code.value,
+            message=exc.message or default_msg
+        )
+    )
 
 
 if __name__ == "__main__":
