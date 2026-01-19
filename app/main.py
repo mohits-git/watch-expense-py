3cf32d8f8454eea599be05200d1648be6651689a
mohits-git <mohitkhatri2805@gmail.com>
2026-01-19 09:40:18 +0000

    fix: proxy headers issue with apigw - nlb - vpclink integration. add api proxy headers middleware v1

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from app.lifespan import lifespan
+from app.middleware import register_middlewares
 from app.routers.auth import auth_router
 from app.routers.user import user_router
 from app.routers.project import project_router
@@ -13,6 +13,7 @@ from app.exception import register_exception_handlers
 
 
 app = FastAPI(lifespan=lifespan)
+register_middlewares(app)
 register_exception_handlers(app)
 app.include_router(auth_router, prefix="/api")
 app.include_router(user_router, prefix="/api")
@@ -21,19 +22,6 @@ app.include_router(department_router, prefix="/api")
 app.include_router(expense_router, prefix="/api")
 app.include_router(advance_router, prefix="/api")
 app.include_router(image_router, prefix="/api")
-
-origins = [
-    "https://watchexpense.mohits.me",
-    "http://localhost:4200"
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 @app.get('/health')
