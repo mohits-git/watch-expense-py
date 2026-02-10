a6604c76d751af235834b522c9db7174a5e6fcfe
mohits-git <mohitkhatri2805@gmail.com>
2026-02-10 15:41:06 +0000

    fix: update build script

--- deploy/build-and-push.sh
+++ deploy/build-and-push.sh
@@ -1,1 +1,5 @@
+echo "Building and Publishing docker image to ECR..."
 docker buildx build --platform linux/amd64 -t 873335417993.dkr.ecr.ap-south-1.amazonaws.com/mohits-ecr/watch-expense-py:latest --push .
+
+echo "Creating a new (force) deployement for service..."
+aws ecs update-service --region ap-south-1 --cluster watch-expense-py-cluster --service watch-expense-py-service --force-new-deployment
